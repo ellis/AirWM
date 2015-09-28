417193a395d279132f92bc2b239b88bc1f14428e
Ellis Whitehead <web99@ellisw.net>
2015-09-28 19:41:44 +0100

    working on immutable state

--- Architecture.md
+++ Architecture.md
@@ -7,6 +7,30 @@ A "BACKGROUND" window fills the container and is at the bottom of the window sta
 there can only be one background window per container.
 A "DOCK" window ...
 
+```yaml
+idNext: 0
+containers:
+  1:
+    name: web
+    children: [5]
+    docks: []
+    background: ID
+    floats: []
+    screen: 0
+    focus: 5
+    ...
+screens:
+  0:
+    desktopCurrent: 1
+panes:
+  5:
+    id: 5
+    parent: 1
+    xid: 0x141241512
+screenCurrent: ID
+paneCurrent: ID
+```
+
 * Screen
 	* list of all workpaces
 	* mru stack of workspace indexes
---
+++ test/coreSpec.js
@@ -0,0 +1,38 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import * as core from '../lib/core.js';
+
+describe('application logic', () => {
+
+	describe('setContainers', () => {
+		it('adds the containers to the state', () => {
+			const state = Map();
+			const containers = Map({
+				0: {
+					name: "web"
+				}
+			});
+			const nextState = core.setContainers(state, containers);
+			expect(nextState).to.equal(Map({
+				containers: containers
+			}));
+		});
+	});
+
+	describe('setScreens', () => {
+		it('adds the screens to the state', () => {
+			const state = Map();
+			const screens = Map({
+				0: {
+					width: 800,
+					height: 600
+				}
+			});
+			const nextState = core.setScreens(state, screens);
+			expect(nextState).to.equal(Map({
+				screens: screens
+			}));
+		});
+	});
+});
