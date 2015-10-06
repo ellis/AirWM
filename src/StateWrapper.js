55d9cf6c022694912e5345b5093fe344c689753b
Ellis Whitehead <ellis@ellisw.net>
2015-10-06 14:23:05 +0200

    StateWrapper tests for addDesktop and addScreen

--- src/StateWrapper.js
+++ src/StateWrapper.js
@@ -5,11 +5,12 @@ import {List, Map, fromJS} from 'immutable';
 export const initialState = fromJS({
 	widgetIdNext: 0,
 	screenIdOrder: [],
-	screenIdChain: [],
 	desktopIdOrder: [],
-	desktopIdChain: [],
 	windowIdOrder: [],
-	windowIdChain: [],
+	widgetIdChain: [],
+	currentScreenId: -1,
+	currentDesktopId: -1,
+	currentWindowId: -1,
 });
 
 const listRemove = (l, x) => l.filter(a => a !== x);
@@ -37,22 +38,26 @@ class SubWrapper {
 }
 
 class DesktopWrapper extends SubWrapper {
-	constructor(top, path) {
+	constructor(top, path, id) {
 		super(top, path, Map());
+		this.id = id;
 		//console.log({top: this.top, path: this.path})
 		assert.equal(this.top._get(this.path.concat(['type'])), 'desktop');
 	}
 
 	get type() { return "desktop"; }
 	get layout() { return this._get('type', 'default'); }
-	get screenId() { return this._get('screenId', -1); }
+	get parentId() { return this._get('parentId', -1); }
 	getChildIdOrder() { return this._get('childIdOrder', List()); }
 	getChildIdChain() { return this._get('childIdChain', List()); }
-	getChildIdStack() { return this._get('childIdStack', List()); }
+	//getChildIdStack() { return this._get('childIdStack', List()); }
+	get currentWindowId() { return this._get(['childIdChain', 0], -1); }
+
+	findScreenId() { return this._get('parentId', -1); }
 
 	_childIdOrder() { return new UniqueListWrapper(this.top, this.path.concat(['childIdOrder'])); }
 	_childIdChain() { return new UniqueListWrapper(this.top, this.path.concat(['childIdChain'])); }
-	_childIdStack() { return new UniqueListWrapper(this.top, this.path.concat(['childIdStack'])); }
+	//_childIdStack() { return new UniqueListWrapper(this.top, this.path.concat(['childIdStack'])); }
 }
 
 class ListWrapper extends SubWrapper {
@@ -67,13 +72,17 @@ class ListWrapper extends SubWrapper {
 }
 
 class ScreenWrapper extends SubWrapper {
-	constructor(top, path) {
+	constructor(top, path, id) {
 		super(top, path, Map());
+		this.id = id;
 	}
 
 	get xid() { return this.top._get(this.path.concat(['xid']), -1); }
 	get width() { return this.top._get(this.path.concat(['width']), 0); }
 	get height() { return this.top._get(this.path.concat(['height']), 0); }
+
+	get currentDesktopId() { return this._get(['desktopIdChain', 0], -1); }
+	get currentDesktop() { return this.top.desktopById(this.currentDesktopId); }
 
 	getDesktopIdChain() { return this._get('desktopIdChain', List()); }
 
@@ -86,17 +95,21 @@ class UniqueListWrapper extends SubWrapper {
 	}
 
 	push(x) { this.top._updateIn(this.path, List(), l => listRemove(l, x).push(x)); return this; }
-	unshift(x) { this.top._updateIn(this.path, List(), l => listRemove(l, x).push(x)); return this; }
-	insert(x, index) { this.top._updateIn(this.path, List(), l => listRemove(l, x).push(x)); return this; }
+	unshift(x) { this.top._updateIn(this.path, List(), l => listRemove(l, x).unshift(x)); return this; }
+	insert(x, index) { this.top._updateIn(this.path, List(), l => listRemove(l, x).splice(index, 0, x)); return this; }
 	remove(x) { this.top._updateIn(this.path, List(), l => listRemove(l, x)); return this; }
 }
 
 export const StatePaths = {
 	widgetIdNext: ['widgetIdNext'],
 	screenIdOrder: ['screenIdChain'],
-	screenIdChain: ['screenIdChain'],
 	desktopIdOrder: ['desktopIdOrder'],
-	desktopIdChain: ['desktopIdChain'],
+	windowIdOrder: ['windowIdOrder'],
+	windowIdStack: ['windowIdStack'],
+	widgetIdChain: ['widgetIdChain'],
+	currentScreenId: ['currentScreenId'],
+	currentDesktopId: ['currentDesktopId'],
+	currentWindowId: ['currentWindowId']
 }
 
 export default class StateWrapper {
@@ -104,21 +117,32 @@ export default class StateWrapper {
 		this.state = fromJS(state);
 
 		this._screenIdOrder = new UniqueListWrapper(this, StatePaths.screenIdOrder);
-		this._screenIdChain = new UniqueListWrapper(this, StatePaths.screenIdChain);
 		this._desktopIdOrder = new UniqueListWrapper(this, StatePaths.desktopIdOrder);
-		this._desktopIdChain = new UniqueListWrapper(this, StatePaths.desktopIdChain);
+		this._windowIdOrder = new UniqueListWrapper(this, StatePaths.windowIdOrder);
+		this._windowIdStack = new UniqueListWrapper(this, StatePaths.windowIdStack);
+		this._widgetIdChain = new UniqueListWrapper(this, StatePaths.widgetIdChain);
+	}
+
+	clone() {
+		return new StateWrapper(this.state);
 	}
 
 	getState() { return this.state; }
 	getWidgetIdNext() { return this.state.getIn(StatePaths.widgetIdNext, 0); }
 	getScreenIdOrder() { return this.state.getIn(StatePaths.screenIdOrder, List()); }
-	getScreenIdChain() { return this.state.getIn(StatePaths.screenIdChain, List()); }
 	getDesktopIdOrder() { return this.state.getIn(StatePaths.desktopIdOrder, List()); }
-	getDesktopIdChain() { return this.state.getIn(StatePaths.desktopIdChain, List()); }
-	getCurrentScreenId() { return this.state.getIn(['screenIdChain', 0], 0); }
+	getWindowIdOrder() { return this.state.getIn(StatePaths.windowIdOrder, List()); }
+	getWidgetIdChain() { return this.state.getIn(StatePaths.widgetIdChain, List()); }
+	//getScreenIdChain() { return this.state.getIn(StatePaths.screenIdChain, List()); }
+	get currentScreenId() { return this.state.getIn(StatePaths.currentScreenId, -1); }
+	get currentDesktopId() { return this.state.getIn(StatePaths.currentDesktopId, -1); }
+	get currentWindowId() { return this.state.getIn(StatePaths.currentWindowId, -1); }
 
-	screenById(screenId) { return new ScreenWrapper(this, ['widgets', screenId.toString()]); }
-	desktopById(desktopId) { return new DesktopWrapper(this, ['widgets', desktopId.toString()]); }
+	screenById(screenId) { return this._widgetById(ScreenWrapper, screenId); }
+	desktopById(desktopId) { return this._widgetById(DesktopWrapper, desktopId); }
+
+	currentScreen() { return this.screenById(this.currentScreenId); }
+	currentDesktop() { return this.desktopById(this.currentDesktopId); }
 
 	addDesktop(w) {
 		w = fromJS(w);
@@ -128,15 +152,15 @@ export default class StateWrapper {
 			layout: "default",
 			childIdOrder: [],
 			childIdChain: [],
-			childIdStack: [],
+			//childIdStack: [],
 		}).merge(w);
 
 		// Add widget to widgets list
 		const id = this._addWidget(w);
 
-		// Append to desktop lists
+		// Append to desktop list
 		this._desktopIdOrder.push(id);
-		this._desktopIdChain.push(id);
+		this._widgetIdChain.push(id);
 
 		// Append to the visit list for all screens
 		this.forEachScreen(screen => {
@@ -148,20 +172,24 @@ export default class StateWrapper {
 
 	addScreen(screen) {
 		const desktopIdOrder0 = this.getDesktopIdOrder().toJS();
-		const desktopIdVisibles = this.mapEachScreen(screen => screen.getCurrentDesktopId());
+		// List of exisitng desktop which are on a screen
+		const desktopIdVisibles = this.mapEachScreen(screen => screen.currentDesktopId);
+		// List of exisitng desktop which aren't currently shown on any screen
 		const desktopIdHiddens = _.difference(desktopIdOrder0, desktopIdVisibles);
 
 		// Figure out which desktop to display
 		let desktopId;
 		// If there aren't and free desktops to display on this screen:
 		if (_.isEmpty(desktopIdHiddens)) {
+			// Create a new desktop for this screen.
 			desktopId = addDesktop({});
 		}
+		// Otherwise, take the first hidden desktop
 		else {
 			desktopId = _.first(desktopIdHiddens);
 		}
 
-		// Chaining order for desktops
+		// Chaining order for desktops, puts chosen desktop at front of list of all desktops
 		const desktopIdChain = [desktopId].concat(_.without(desktopIdOrder0, desktopId));
 
 		let w = fromJS(screen);
@@ -176,13 +204,146 @@ export default class StateWrapper {
 
 		// Append to screen lists
 		this._screenIdOrder.push(id);
-		this._screenIdChain.push(id);
+		this._widgetIdChain.push(id);
 
-		// Set desktop's screenId
+		// Set desktop's parentId
 		//console.log({desktopId, desktop: this.desktopById(desktopId), id, klass: typeof this.desktopById(desktopId)})
-		this.desktopById(desktopId)._set('screenId', id);
+		const desktop = this.desktopById(desktopId);
+		desktop._set('parentId', id);
+
+		// If this is the first screen, set it as current:
+		if (this.currentScreenId < 0)
+			this._setCurrent(id);
 
 		return id;
+	}
+
+	/**
+	 * Add a window to the state.
+	 * It won't have a parent and will be put at the end of the window lists.
+	 * @param {object} w - object describing a window
+	 * @param {number} w.xid - the X11 window ID
+	 * @returns ID of new window
+	 */
+	addWindow(w) {
+		w = fromJS(w);
+		// Default values
+		w = fromJS({
+			type: 'window',
+			parentId: -1,
+			state: {}
+		}).merge(w);
+
+		// Add widget to widgets list
+		const id = this._addWidget(w);
+
+		// Append to window lists
+		this._windowIdOrder.push(id);
+		this._windowIdStack.push(id);
+		this._widgetIdChain.push(id);
+
+		return id;
+	}
+
+	/**
+	 * Remove window from desktop.  Window will not be visible.
+	 *
+	 * @param  {number} windowId
+	 */
+	unparentWindow(windowId) {
+		// Find window's desktop
+		const desktopId = this._findWidgetDekstopIdById(windowId);
+		// Set window's parent to -1
+		this._set(['widgets', windowId.toString(), 'parentId'], -1);
+		// Remove window from desktop's window chain
+		if (desktopId >= 0) {
+			const desktop = this.desktopById(desktopId);
+			desktop._childIdOrder.remove(windowId);
+			desktop._childIdChain.remove(windowId);
+			//desktop._childIdStack.remove(windowId);
+			this._setCurrent();
+		}
+		return this;
+	}
+
+	moveDesktopToScreen(desktop, screen) {
+		if (_.isNumber(desktop))
+			desktop = this.desktopById(desktop);
+		if (_.isNumber(screen))
+			screen = this.screenById(screen);
+
+		if (desktop && screen) {
+			const screenPrev = this.screenById(desktop.findScreenId());
+			const desktopPrev = this.desktopById(screen.currentDesktopId);
+			// If the desktop was already displayed on another screen
+			if (screenPrev && screenPrev.id !== screen.id) {
+				// Move desktop on this screen to that screen
+				desktopPrev._set('parentId', screenPrev.id);
+				screenPrev._desktopIdChain.unshift(desktopPrev.id);
+			}
+			// Place desktop on screen
+			desktop._set('parentId', screen.id);
+			// Move desktop to head of screen's stack
+			screen._desktopIdChain.unshift(desktop.id);
+			// Update current pointers
+			this._setCurrent();
+		}
+		return this;
+	}
+
+	moveWindowToDesktop(windowId, desktopId) {
+		// Remove window from old parent
+		this.unparentWindow(windowId);
+		// Add window to given desktop
+		if (desktopId >= 0) {
+			// Set parent ID
+			this._set(['widgets', windowId.toString(), 'parentId'], desktopId);
+			// Append to desktop's child list
+			if (desktopId >= 0) {
+				const desktop = this.desktopById(desktopId);
+				desktop._childIdOrder.push(windowId);
+				desktop._childIdChain.push(windowId);
+				//desktop._childIdStack.push(windowId);
+			}
+		}
+		return this;
+	}
+
+	activateScreen(screen) {
+		this._setCurrent(screen);
+		return this;
+	}
+
+	activateDesktop(desktop) {
+		if (_.isNumber(desktop))
+			desktop = this.desktopById(desktopId);
+		if (desktop) {
+			// If the desktop is already on a screen, activate that screen
+			let screenId = desktop.findScreenId();
+			if (screenId >= 0) {
+				this._setCurrent(screenId);
+			}
+			// Otherwise, move the desktop to the current screen.
+			else {
+				this.moveDesktopToScreen(desktop, currentScreen);
+				this._setCurrent();
+			}
+		}
+		return this;
+	}
+
+	activateWindow(windowId) {
+		// Find desktop
+		const desktop = desktopById(this._findWidgetDekstopIdById(windowId));
+		if (desktop) {
+			// Move window to front on desktop's chain
+			desktop._windowIdChain.unshift(windowId);
+			// And activate the desktop
+			this.activateDesktop(desktop);
+		}
+		//
+		// Activate desktop
+		return this;
 	}
 
 	forEachScreen(fn) {
@@ -190,6 +351,7 @@ export default class StateWrapper {
 			const screen = screenById(screenId);
 			fn(screen, screenId);
 		});
+		return this;
 	}
 
 	mapEachScreen(fn) {
@@ -201,20 +363,48 @@ export default class StateWrapper {
 		return result;
 	}
 
+	/* Private helper functions */
+
+	_has(path) {
+		if (_.isString(path))
+			path = [path];
+		return this.state.hasIn(path);
+	}
 
 	_get(path, dflt) {
+		if (_.isString(path))
+			path = [path];
 		return this.state.getIn(path, dflt);
 	}
 
 	_set(path, value) {
+		if (_.isString(path))
+			path = [path];
 		this.state = this.state.setIn(path, value);
 		return this;
 	}
 
 	_updateIn(path, dflt, fn) {
+		if (_.isString(path))
+			path = [path];
 		//console.log({path, dflt, fn})
 		this.state = this.state.updateIn(path, dflt, fn);
 		return this;
+	}
+
+	_widgetById(prototype, id) {
+		if (id >= 0) {
+			const path = this._widgetPath(id);
+			if (this._has(path))
+				return new prototype(this, path, id);
+		}
+		return undefined;
+	}
+
+	_widgetPath(id, path) {
+		if (_.isString(path)) return ['widgets', id.toString(), path];
+		else if (_.isArray(path)) return ['widgets', id.toString()].concat(path);
+		else return ['widgets', id.toString()];
 	}
 
 	_addWidget(w) {
@@ -228,8 +418,49 @@ export default class StateWrapper {
 		return id;
 	}
 
+	_findWidgetDekstopIdById(widgetId) {
+		const w = _getWidgetById(widgetId);
+		if (w.get('type') === 'desktop') {
+			return widgetId;
+		}
+		const parentId = w.get('parentId', -1);
+		if (parentId >= 0) {
+			return this._findWidgetDekstopIdById(parentId);
+		}
+		else {
+			return -1;
+		}
+	}
+
 	_getWidgetById(id) {
 		return this.state.getIn(['widgets', id.toString()]);
+	}
+
+	/**
+	 * Set the current screen.
+	 * This will
+	 * @param {(number|ScreenWrapper)} [screen] - the screen to set as current
+	 */
+	_setCurrent(screen) {
+		if (_.isNumber(screen))
+			screen = this.screenById(screen);
+		else if (_.isUndefined(screen))
+			screen = this.currentScreen;
+
+		if (screen) {
+			const desktop = screen.currentDesktop;
+			// Update the widgetIdChain
+			this._widgetIdChain
+				.unshift(screen.id)
+				.unshift(desktop.id)
+				.unshift(desktop.currentWindowId);
+			// Update the current pointers
+			this
+				._set('currentScreenId', screen.id)
+				._set('currentDesktopId', desktop.id)
+				._set('currentWidowId', desktop.currentWindowId);
+		}
+		return this;
 	}
 
 	/*
--- test/StateWrapperSpec.js
+++ test/StateWrapperSpec.js
@@ -5,41 +5,118 @@ import diff from 'immutablediff';
 import StateWrapper, {initialState} from '../src/StateWrapper.js';
 
 describe('StateWrapper', () => {
-	it('test1', () => {
+	let builder0, builder1, builder2, builder3;
+	let d1, d2, s1;
+	before(() => {
 		const builder = new StateWrapper(initialState);
-		expect(builder.getState()).to.equal(initialState);
-
+		builder0 = builder.clone();
 		// Add desktop 1
-		const d1 = builder.addDesktop({});
-		expect(builder.getWidgetIdNext()).to.equal(d1 + 1);
-		expect(builder.getDesktopIdOrder()).to.equal(List.of(d1));
-		expect(builder.getDesktopIdChain()).to.equal(List.of(d1));
-		expect(builder.desktopById(d1).screenId).to.equal(-1);
+		d1 = builder.addDesktop({});
+		builder1 = builder.clone();
 		// Add desktop 2
-		const d2 = builder.addDesktop({});
-		expect(builder.getWidgetIdNext()).to.equal(d2 + 1);
-		expect(builder.getDesktopIdOrder()).to.equal(List.of(d1, d2));
-		expect(builder.getDesktopIdChain()).to.equal(List.of(d1, d2));
-		expect(builder.desktopById(d1).screenId).to.equal(-1);
-		expect(builder.desktopById(d2).screenId).to.equal(-1);
+		d2 = builder.addDesktop({});
+		builder2 = builder.clone();
 		// Add screen 1
-		const s1 = builder.addScreen({
+		s1 = builder.addScreen({
 			xid: 100,
 			width: 800,
 			height: 600,
 		});
-		expect(builder.getWidgetIdNext()).to.equal(s1 + 1);
-		expect(builder.getDesktopIdOrder()).to.equal(List.of(d1, d2));
-		expect(builder.getDesktopIdChain()).to.equal(List.of(d1, d2));
-		expect(builder.getScreenIdOrder()).to.equal(List.of(s1));
-		expect(builder.getScreenIdChain()).to.equal(List.of(s1));
-		expect(builder.screenById(s1).getDesktopIdChain()).to.equal(List.of(d1, d2));
-		expect(builder.desktopById(d1).screenId).to.equal(s1);
-		expect(builder.desktopById(d2).screenId).to.equal(-1);
+		builder3 = builder.clone();
+	});
 
-		//expect(builder.desktopIdOrder.getState()).to.equal(List.of(1,2,3));
+	it('empty', () => {
+		const builder = builder0;
+		expect(builder.getState()).to.equal(initialState);
+	});
 
-		//builder.desktopIdOrder.push(4);
-		//expect(builder.getState().get('desktopIdOrder')).to.equal(List.of(1,2,3,4));
+	describe('addDesktop', () => {
+		describe('with no screens, one desktop', () => {
+			const builder = new StateWrapper(initialState);
+			const d1 = builder.addDesktop({});
+
+			//const builderPrev = builder0;
+			it('should increment `widgetIdNext`', () => {
+				expect(builder.getWidgetIdNext()).to.equal(d1 + 1);
+			});
+			it('should leave the screen order list unchanged', () => {
+				expect(builder.getScreenIdOrder()).to.equal(List());
+			});
+			it('should append to the desktop order list', () => {
+				expect(builder.getDesktopIdOrder()).to.equal(List.of(d1));
+			});
+			it('should leave the winder order list unchanged', () => {
+				expect(builder.getWindowIdOrder()).to.equal(List());
+			});
+			it('should append to the widget chain list', () => {
+				expect(builder.getWidgetIdChain()).to.equal(List.of(d1));
+			});
+			it('should leave the focus references unchanged', () => {
+				expect(builder.currentScreenId).to.equal(-1);
+				expect(builder.currentDesktopId).to.equal(-1);
+				expect(builder.currentWindowId).to.equal(-1);
+			});
+		});
+
+		describe('with no screens, two desktops', () => {
+			let builder, d1, d2, s1;
+			const builder = new StateWrapper(initialState);
+			const d1 = builder.addDesktop({});
+			const d2 = builder.addDesktop({});
+			it('should increment `widgetIdNext`', () => {
+				expect(builder.getWidgetIdNext()).to.equal(d2 + 1);
+			});
+			it('should leave the screen order list unchanged', () => {
+				expect(builder.getScreenIdOrder()).to.equal(List());
+			});
+			it('should append to the desktop order list', () => {
+				expect(builder.getDesktopIdOrder()).to.equal(List.of(d1, d2));
+			});
+			it('should leave the winder order list unchanged', () => {
+				expect(builder.getWindowIdOrder()).to.equal(List());
+			});
+			it('should append to the widget chain list', () => {
+				expect(builder.getWidgetIdChain()).to.equal(List.of(d1, d2));
+			});
+			it('should leave the focus references unchanged', () => {
+				expect(builder.currentScreenId).to.equal(-1);
+				expect(builder.currentDesktopId).to.equal(-1);
+				expect(builder.currentWindowId).to.equal(-1);
+			});
+		});
+	}
+
+	describe('with one screen, two desktops', () => {
+		let builder, d1, d2, s1;
+		before(() => {
+			builder = new StateWrapper(initialState);
+			d1 = builder.addDesktop({});
+			d2 = builder.addDesktop({});
+			s1 = builder.addScreen({
+				xid: 100,
+				width: 800,
+				height: 600,
+			});
+		});
+		it('should increment `widgetIdNext`', () => {
+			expect(builder.getWidgetIdNext()).to.equal(s1 + 1);
+		});
+		it('should append to the screen order list', () => {
+			expect(builder.getScreenIdOrder()).to.equal(List.of(s1));
+		});
+		it('should leave the desktop order list unchanged', () => {
+			expect(builder.getDesktopIdOrder()).to.equal(List.of(d1, d2));
+		});
+		it('should leave the winder order list unchanged', () => {
+			expect(builder.getWindowIdOrder()).to.equal(List());
+		});
+		it('should update the widget chain list', () => {
+			expect(builder.getWidgetIdChain()).to.equal(List.of(-1, d1, s1, d2));
+		});
+		it('should update the focus references', () => {
+			expect(builder.currentScreenId).to.equal(s1);
+			expect(builder.currentDesktopId).to.equal(d1);
+			expect(builder.currentWindowId).to.equal(-1);
+		});
 	});
 });
