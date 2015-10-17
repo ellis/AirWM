92bc27e689908cce66a260639c761b9ca0f8c091
Ellis Whitehead <web99@ellisw.net>
2015-10-17 19:48:38 +0100

    finished moveWindowSpec

--- test/actions/moveWindowSpec.js
+++ test/actions/moveWindowSpec.js
@@ -2,46 +2,36 @@ import {List, Map, fromJS} from 'immutable';
 import {expect} from 'chai';
 import diff from 'immutablediff';
 
+import checkList from '../checkList.js';
 import reducer from '../../src/reducer.js';
-import State from '../../src/state.js';
+import StateWrapper from '../../src/StateWrapper.js';
 import * as ex from '../exampleStates.js';
 
 describe('moveWindowToDesktop', () => {
-	let state1, state2;
-	before(() => {
+	it("works", () => {
+		const [d1, d2, s1, dock1, w1, w2, w3] = [0, 1, 2, 3, 4, 5];
 		let state = ex.state120;
 		// Create three windows
-		state = reducer(state, {type: 'createWidget', widget: {type: 'window', xid: 1001}});
-		state = reducer(state, {type: 'createWidget', widget: {type: 'window', xid: 1002}});
-		state = reducer(state, {type: 'createWidget', widget: {type: 'window', xid: 1003}});
+		state = reducer(state, {type: 'addWindow', window: {type: 'window', xid: 1001}});
+		state = reducer(state, {type: 'addWindow', window: {type: 'window', xid: 1002}});
+		state = reducer(state, {type: 'addWindow', window: {type: 'window', xid: 1003}});
 		// Move window 1 to desktop 2
 		state = reducer(state, {type: 'moveWindowToDesktop', desktop: 1});
-		state1 = state;
+
+		{
+			const builder = new StateWrapper(state);
+			//builder.print();
+			expect(builder.windowById(w1).parentId, "window 1's parent").to.equal(d2);
+			// Don't follow the window
+			expect(builder.currentWindowId, "currentWindowId").to.equal(w2);
+		}
+
 		// Move window 2 to desktop 2
-		state = reducer(state, {type: 'activateDesktop', num: 0});
 		state = reducer(state, {type: 'moveWindowToDesktop', desktop: 1});
-		state2 = state;
+
+		{
+			const builder = new StateWrapper(state);
+			expect(builder.windowById(w2).parentId, "window 2's parent").to.equal(d2);
+		}
 	})
-	it('moves window 1 to desktop 2', () => {
-		const state = state1;
-		expect(state.getIn(['widgets', '0', 'childIdOrder'])).to.equal(List.of(4, 5));
-		expect(state.getIn(['widgets', '0', 'childIdStack'])).to.equal(List.of(5, 4));
-		expect(state.getIn(['widgets', '1', 'childIdOrder'])).to.equal(List.of(3));
-		expect(state.getIn(['widgets', '1', 'childIdStack'])).to.equal(List.of(3));
-		expect(state.getIn(['windowIdOrder'])).to.equal(List.of(3, 4, 5));
-		expect(state.getIn(['windowIdStack'])).to.equal(List.of(3, 5, 4));
-	});
-	it('focuses window 1 on desktop 2', () => {
-		expect(State.getCurrentWindowId(state1)).to.equal(3);
-	});
-	it('moves windows 1 & 2 to desktop 2', () => {
-		const state = state2;
-		expect(state.getIn(['widgets', '0', 'childIdOrder'])).to.equal(List.of(4));
-		expect(state.getIn(['widgets', '0', 'childIdStack'])).to.equal(List.of(4));
-		expect(state.getIn(['widgets', '1', 'childIdOrder'])).to.equal(List.of(3, 5));
-		expect(state.getIn(['widgets', '1', 'childIdStack'])).to.equal(List.of(5, 3));
-		expect(state.getIn(['windowIdOrder'])).to.equal(List.of(3, 4, 5));
-		expect(state.getIn(['windowIdStack'])).to.equal(List.of(5, 3, 4));
-		expect(State.getCurrentWindowId(state)).to.equal(5);
-	});
 });
