5a1c5c1353d2e3a57a3d128f771bdd345d7f969a
Ellis Whitehead <ellis@ellisw.net>
2017-08-06 12:51:13 +0200

    test3: displaying desktop as an SVG

---
+++ elm/test3.elm
@@ -0,0 +1,84 @@
+import Html exposing (..)
+import Html.Events exposing (..)
+import Random
+import Svg exposing (svg, polygon, rect)
+import Svg.Attributes exposing (..)
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+-- MODEL
+
+type alias Model =
+  { desktops: List DesktopWidget
+  }
+
+type alias Rect =
+  { x: Int, y: Int, w: Int, h: Int }
+
+type alias DesktopWidget =
+  { name: String
+  , parentId: Maybe Int
+  , layout: String
+  , rc: Rect
+  , childIdOrder: List Int
+  , childIdChain: List Int
+  }
+
+init : (Model, Cmd Msg)
+init =
+  let
+    desktops =
+      [ DesktopWidget "web" Nothing "tile-right" (Rect 0 25 1920 1055) [] []
+      ]
+  in
+    (Model desktops, Cmd.none)
+
+-- UPDATE
+
+type Msg =
+  AddDesktop
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+  AddDesktop ->
+    (model, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+  (List.concat
+    [ [ h1 [] [ text (toString (List.length model.desktops)) ]
+      , button [ onClick AddDesktop ] [ text "Add Desktop" ]
+      ]
+    , [makeSvgViewH model]
+    ]
+  )
+
+-- Display desktops horizontally
+makeSvgViewH : Model -> Html Msg
+makeSvgViewH model =
+    svg
+      [ version "1.1", x "0", y "0", viewBox "0 0 600 400", style "width: 15em; float: left" ]
+      (List.concat (List.map makeSvgDesktop model.desktops))
+
+makeSvgDesktop : DesktopWidget -> List (Svg.Svg Msg)
+makeSvgDesktop desktop =
+    [ rect [width (toString desktop.rc.w), height (toString desktop.rc.h)] []
+    , Svg.text_ [] [Svg.text desktop.name]
+    ]
